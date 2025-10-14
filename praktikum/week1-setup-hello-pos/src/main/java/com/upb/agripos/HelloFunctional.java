691543295564ce0038ba27e847187afc231b5836
AganChois <agancho930@gmail.com>
2025-10-14 07:44:53 +0700

    week1-setup-hello-pos

---
+++ praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/HelloFunctional.java
@@ -0,0 +1,19 @@
+package main.java.com.upb.agripos;
+
+import java.util.*;
+import java.util.stream.*;
+public class HelloFunctional {
+   public static void main(String[] args) {
+      String nim = "240202893";
+      String nama = "Agan Chois";
+      List<String> produk = Arrays.asList("Beras", "Pupuk", "Benih");
+      List<Integer> harga = Arrays.asList(25000, 50000, 35000);
+      System.out.println("Hello POS World");
+      System.out.println("NIM: " + nim + ", Nama: " + nama);
+      System.out.println("Daftar Produk:");
+      IntStream.range(0, produk.size())
+         .forEach(i -> System.out.println("- " + produk.get(i) + ": " + harga.get(i)));
+      int total = harga.stream().mapToInt(Integer::intValue).sum();
+      System.out.println("Total harga semua produk: " + total);
+   }
+}
---
+++ praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/HelloOOP.java
@@ -0,0 +1,31 @@
+package main.java.com.upb.agripos;
+
+class Produk {
+   String nama;
+   int harga;
+   Produk(String nama, int harga) {
+      this.nama = nama;
+      this.harga = harga;
+   }
+}
+
+public class HelloOOP {
+   public static void main(String[] args) {
+      String nim = "240202893";
+      String namaMhs = "Agan Chois";
+      Produk[] daftar = {
+         new Produk("Beras", 25000),
+         new Produk("Pupuk", 50000),
+         new Produk("Benih", 35000)
+      };
+      int total = 0;
+      System.out.println("Hello POS World");
+      System.out.println("NIM: " + nim + ", Nama: " + namaMhs);
+      System.out.println("Daftar Produk:");
+      for (Produk p : daftar) {
+         System.out.println("- " + p.nama + ": " + p.harga);
+         total += p.harga;
+      }
+      System.out.println("Total harga semua produk: " + total);
+   }
+}
